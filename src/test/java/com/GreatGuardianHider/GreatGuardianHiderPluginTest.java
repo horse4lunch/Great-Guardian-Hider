c2742992375cb3c3a7fd3cb63d4c52b31a6e3bc9
yomomma <jezzy.maxwell@gmail.com>
2023-07-07 10:31:45 -0400

    Renamed ExamplePluginTest.java to GreatGuardianHiderPluginTest.java

--- src/test/java/com/GreatGuardianHider/GreatGuardianHiderPluginTest.java
+++ src/test/java/com/GreatGuardianHider/GreatGuardianHiderPluginTest.java
@@ -3,7 +3,7 @@ package com.GreatGuardianHider;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GreatGuardianHiderPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
